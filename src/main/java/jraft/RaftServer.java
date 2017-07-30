7a44eeb03b2b1b9c5caa18aadfa80887ec195056
lichen970 <lichen970@gmail.com>
2017-07-29 18:01:21 -0700

    polish raft server context

--- src/main/java/jraft/RaftServer.java
+++ src/main/java/jraft/RaftServer.java
@@ -1,7 +1,13 @@
 package jraft;
 
+import java.util.Collection;
+
 /**
  * Created by Chen on 7/20/17.
  */
 public interface RaftServer {
+
+    void bootstrap(Collection<String> serverNames);
+
+    void shutdown();
 }
---
+++ src/main/java/jraft/impl/RaftServerImpl.java
@@ -0,0 +1,20 @@
+package jraft.impl;
+
+import jraft.RaftServer;
+
+import java.util.Collection;
+
+/**
+ * Created by Chen on 7/29/17.
+ */
+public class RaftServerImpl implements RaftServer {
+    @Override
+    public void bootstrap(Collection<String> serverNames) {
+
+    }
+
+    @Override
+    public void shutdown() {
+
+    }
+}
